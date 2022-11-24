d6da693e6917c2c450691f41a97ad77df5d09008
Shuhei Yoshida <aneki525@gmail.com>
2022-11-24 13:45:24 +0900

    update feature

--- lib/common/model/feature.dart
+++ lib/common/model/feature.dart
@@ -1,8 +1,8 @@
 // ignore: depend_on_referenced_packages
 import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:auto_route/auto_route.dart';
 import 'package:shus_flutter/common/enum/platform_type.dart';
 import 'package:shus_flutter/common/enum/tech.dart';
-import 'package:shus_flutter/common/router/route_path.dart';
 
 part 'feature.freezed.dart';
 
@@ -11,7 +11,7 @@ class Feature with _$Feature {
   factory Feature({
     @Default('') String title,
     @Default('') String desctiption,
-    @Default(RoutePath.init) RoutePath path,
+    required PageRouteInfo route,
     DateTime? uploadAt,
     @Default([]) List<PlatformType> platformTypes,
     @Default([]) List<Tech> techs,
