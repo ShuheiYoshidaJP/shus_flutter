67d58d8c15ebbe2caa8ecac13eaa1b014e74fbb2
Shuhei Yoshida <aneki525@gmail.com>
2022-11-24 13:45:31 +0900

    update main

--- lib/main.dart
+++ lib/main.dart
@@ -2,17 +2,19 @@ import 'package:flutter/material.dart';
 import 'common/router/app_router.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  MyApp({Key? key}) : super(key: key);
+  final _appRouter = AppRouter();      
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
-      routerConfig: appRouter,
+      routerDelegate: _appRouter.delegate(),      
+      routeInformationParser: _appRouter.defaultRouteParser(), 
     );
   }
 }
