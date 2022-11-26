ffeee1f380ac3dca8e400430f08af72b4c40fd3c
Shuhei Yoshida <aneki525@gmail.com>
2022-11-26 17:49:17 +0900

    update styles

--- lib/common/styles.dart
+++ lib/common/styles.dart
@@ -11,4 +11,14 @@ class Styles {
     fontSize: 16,
     fontWeight: FontWeight.w500,
   );
+  static const TextStyle input = TextStyle(
+    color: Colors.black54,
+    fontSize: 24,
+    fontWeight: FontWeight.w700,
+  );
+  static const TextStyle inputSuffix = TextStyle(
+    color: Colors.black54,
+    fontSize: 16,
+    fontWeight: FontWeight.w700,
+  );
 }
