2b917c184dee4ed6f899fd4cbb2a09c5a65c3b7d
Shuhei Yoshida <aneki525@gmail.com>
2022-11-26 14:57:30 +0900

    add space

---
+++ lib/common/ui_components/space.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class Space {
+  static const h4 = SizedBox(height: 4);
+  static const h8 = SizedBox(height: 8);
+  static const h12 = SizedBox(height: 12);
+  static const h16 = SizedBox(height: 16);
+  static const h20 = SizedBox(height: 20);
+  static const h24 = SizedBox(height: 24);
+  static const hInf = SizedBox(height: double.infinity);
+  static const w4 = SizedBox(width: 4);
+  static const w8 = SizedBox(width: 8);
+  static const w12 = SizedBox(width: 12);
+  static const w16 = SizedBox(width: 16);
+  static const w20 = SizedBox(width: 20);
+  static const w24 = SizedBox(width: 24);
+  static const wInf = SizedBox(width: double.infinity);
+}
