eda69873ac13322cf9a95de9c592caf88bf9eaf7
Shuhei Yoshida <aneki525@gmail.com>
2022-11-26 14:58:44 +0900

    add feature list page

--- lib/feature/feature_list/feature_list_page.dart
+++ lib/feature/feature_list/feature_list_page.dart
@@ -1,7 +1,7 @@
-import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:shus_flutter/common/model/feature.dart';
 import 'package:shus_flutter/feature/feature_list/all_feature.dart';
+import 'package:shus_flutter/feature/feature_list/feature_list_cell.dart';
 
 class FeatureListPage extends StatefulWidget {
   const FeatureListPage({Key? key}) : super(key: key);
@@ -16,27 +16,15 @@ class _FeatureListPageState extends State<FeatureListPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text('feature list'),
-            const SizedBox(
-              height: 16,
-            ),
-            Text(
-              'current route path: ${ModalRoute.of(context)?.settings.name}',
-            ),
-            const SizedBox(
-              height: 16,
-            ),
-            TextButton(
-              child: const Text('next'),
-              onPressed: () => context.pushRoute(featureList[0].route),
-            ),
-          ],
+      body: ListView.builder(
+          shrinkWrap: true,
+          itemCount: featureList.length,
+          physics: const AlwaysScrollableScrollPhysics(),
+          itemBuilder: (context, index) {
+            var feature = featureList[index];
+            return FeatureListCell(feature: feature);
+          },
         ),
-      ),
     );
   }
 }
