4a60ca0b2b0f42ed158c6df7a017c24d305ac1fd
Shuhei Yoshida <aneki525@gmail.com>
2022-11-29 01:14:42 +0900

    update native plugin

--- ios/Runner/Interface/NativePlugin.swift
+++ ios/Runner/Interface/NativePlugin.swift
@@ -8,18 +8,21 @@
 import Foundation
 
 class NativePlugin {
-    let poseMain = PoseMain()
+    let poseMain = PoseMain()    
     
-    var completePose: ((NativeRusultBase) -> Void)?
-    
-    func poseDetect(poseInput: PoseInput) -> [String: Any?] {
-        let uiImage = poseInput.image?.toUIImage()
-        let result = poseMain.detect(uiImage: uiImage)
-        switch result {
-        case .success(let output):
-            return output.toDict()
-        case .failure(let error):
-            return error.toDict()
+    func poseDetect(poseInput: PoseInput,
+                    detectComleted: @escaping ([String: Any?]) -> () ) {
+        let uIImage = poseInput.image?.uIImage
+        DispatchQueue.background(input: uIImage) { uIImage -> Result<PoseOutput, PoseError> in
+            let result = self.poseMain.detect(uiImage: uIImage)
+            return result
+        } completion: { result in
+            switch result {
+            case .success(let output):
+                detectComleted(output.toDict())
+            case .failure(let error):
+                detectComleted(error.toDict())
+            }
         }
     }
 }
