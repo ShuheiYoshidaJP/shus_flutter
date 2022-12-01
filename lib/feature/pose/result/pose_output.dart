48c3ce0f6bd5e07ca2c9a41a97e8c8d94fce7a93
Shuhei Yoshida <aneki525@gmail.com>
2022-12-01 16:20:50 +0900

    update pose output

--- lib/feature/pose/result/pose_output.dart
+++ lib/feature/pose/result/pose_output.dart
@@ -1,13 +1,25 @@
 import 'package:shus_flutter/common/interface/flutter_image.dart';
+import 'package:shus_flutter/feature/pose/pose.dart';
+import 'package:shus_flutter/feature/pose/pose_3d.dart';
+import 'package:shus_flutter/feature/pose/pose_body_part_type.dart';
+
 class PoseOutput {
   FlutterImageBase image;
-  //TODO: Poseクラス入れる
+  List<Pose> poseList;
 
-  PoseOutput({required this.image});
+  PoseOutput({required this.image, required this.poseList});
 
   factory PoseOutput.fromData(Map<String, dynamic> args) {
     final imageArgs = Map<String, dynamic>.from(args["image"]);
-    //TODO: add Pose
-    return PoseOutput(image: FlutterByteImage.fromData(imageArgs));
+    final poseData = Map<String, dynamic>.from(args["pose"]);
+    final poseArray = PoseBodyPartType.values.map((type) {
+      var mapData = Map<String, dynamic>.from(poseData[type.responseKey]);
+      var point = Pose3d.fromData(mapData);
+      return Pose(type: type, point: point);
+    }).toList();
+    return PoseOutput(
+      image: FlutterByteImage.fromData(imageArgs),
+      poseList: poseArray,
+    );
   }
 }
