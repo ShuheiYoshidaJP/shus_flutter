29fc2da2c336b28a072efa46e41f25fd958286bf
Shuhei Yoshida <aneki525@gmail.com>
2022-12-03 10:13:57 +0900

    convert input image

--- android/app/src/main/kotlin/com/example/shus_flutter/common/data/FlutterImage.kt
+++ android/app/src/main/kotlin/com/example/shus_flutter/common/data/FlutterImage.kt
@@ -2,6 +2,7 @@ package com.example.shus_flutter.common.data
 
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
+import com.google.mlkit.vision.common.InputImage
 import java.util.*
 
 enum class FileType {
@@ -11,7 +12,7 @@ enum class FileType {
 class FlutterImage() {
 
     lateinit var type: FileType
-    lateinit var image: Bitmap
+    lateinit var bitmap: Bitmap
 
     constructor(data: HashMap<String, Any>) : this() {
         val typeStr = data["fileType"] as String
@@ -20,7 +21,7 @@ class FlutterImage() {
             "bytes" -> FileType.Bytes
             else -> throw UnknownError()
         }
-        image = when (type) {
+        bitmap = when (type) {
             FileType.File -> {
                 val filePath = data["path"] as String
 //                val imgFile = File(filePath)
@@ -31,6 +32,10 @@ class FlutterImage() {
                 BitmapFactory.decodeByteArray(content, 0, content.size)
             }
         }
+    }
+
+    fun toInputImage(): InputImage {
+        return InputImage.fromBitmap(bitmap, 0)
     }
 }
 
