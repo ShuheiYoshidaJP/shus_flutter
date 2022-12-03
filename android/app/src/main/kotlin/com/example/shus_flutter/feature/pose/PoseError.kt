101376f74b7b8600652d3703951903c83dbdfa11
Shuhei Yoshida <aneki525@gmail.com>
2022-12-03 15:15:24 +0900

    pose error inherit native error

--- android/app/src/main/kotlin/com/example/shus_flutter/feature/pose/PoseError.kt
+++ android/app/src/main/kotlin/com/example/shus_flutter/feature/pose/PoseError.kt
@@ -1,7 +1,19 @@
 package com.example.shus_flutter.feature.pose
 
-enum class PoseError(val code: Int, val message: String) {
-    DETECT(0, "During detecting pose occured error"),
-    IMAGENOTFOUND(1,"UIImage not found"),
-    UNEXPEXTED(2,"Unexpected Error"),
+import com.example.shus_flutter.common.`interface`.NativeError
+import java.util.*
+
+enum class PoseError(override val code: Int, override val message: String) : NativeError {
+
+    DETECT(0, "During detecting pose occurred error"),
+    IMAGENOTFOUND(1, "UIImage not found"),
+    UNEXPEXTED(2, "Unexpected Error");
+
+    override fun toData(): HashMap<String, Any> {
+        var result: HashMap<String, Any> = HashMap<String, Any>()
+        result.put("result", isSuccess)
+        result.put("code", code)
+        result.put("message", message)
+        return  result
+    }
 }
