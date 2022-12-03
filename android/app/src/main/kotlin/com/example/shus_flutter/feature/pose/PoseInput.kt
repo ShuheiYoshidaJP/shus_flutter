93f705742b153bf5ead2bea9fe4e3c356f52bbfc
Shuhei Yoshida <aneki525@gmail.com>
2022-12-03 15:16:38 +0900

    update pose input

--- android/app/src/main/kotlin/com/example/shus_flutter/feature/pose/PoseInput.kt
+++ android/app/src/main/kotlin/com/example/shus_flutter/feature/pose/PoseInput.kt
@@ -4,13 +4,15 @@ import com.example.shus_flutter.common.data.FlutterImage
 import java.util.*
 
 class PoseInput {
-    var height: Double? = data["height"]
+    var height: Double? = null
     var image: FlutterImage? = null
 
-    constructor(data: HashMap<String, Any>){
-        val imageData = data["image"]
-         if (imageData is HashMap<*, *>) {
-             image = FlutterImage(imageData)
-         }
+    constructor(data: HashMap<String, Any>?) {
+        if (data == null) return
+        height = data["height"] as Double?
+        val imageData: HashMap<String, Any>? = data["image"] as? HashMap<String, Any>?
+        if (imageData != null) {
+            image =  FlutterImage(imageData)
+        }
     }
 }
