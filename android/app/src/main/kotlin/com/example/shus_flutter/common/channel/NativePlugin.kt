9fca55220a6300c340e701b0dd863c0fcc60697e
Shuhei Yoshida <aneki525@gmail.com>
2022-12-04 20:24:53 +0900

    update native plugin

--- android/app/src/main/kotlin/com/example/shus_flutter/common/channel/NativePlugin.kt
+++ android/app/src/main/kotlin/com/example/shus_flutter/common/channel/NativePlugin.kt
@@ -4,9 +4,17 @@ import com.example.shus_flutter.feature.pose.PoseChannel
 import java.util.*
 
 class NativePlugin {
-    fun poseDetect(args: Any) {
+    fun poseDetect(args: Any, completion: (HashMap<String, Any>) -> Unit) {
         val poseChannel = PoseChannel()
         val argument: HashMap<String, Any>? = args as? HashMap<String, Any>?
-        poseChannel.poseDetect(argument)
+        poseChannel.poseDetect(argument) { poseResult ->
+            if (poseResult.isSuccess) {
+                val data = poseResult.success?.toData()
+                completion(data!!)
+            } else {
+                val data = poseResult.failure?.toData()
+                completion(data!!)
+            }
+        }
     }
 }
