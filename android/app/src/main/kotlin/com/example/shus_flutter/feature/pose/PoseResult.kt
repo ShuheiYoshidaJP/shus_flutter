10e8b2a0e2bfa4692eb8ac3de81949c66a175966
Shuhei Yoshida <aneki525@gmail.com>
2022-12-04 20:24:33 +0900

    update pose channel

--- android/app/src/main/kotlin/com/example/shus_flutter/feature/pose/PoseResult.kt
+++ android/app/src/main/kotlin/com/example/shus_flutter/feature/pose/PoseResult.kt
@@ -1,6 +1,8 @@
 package com.example.shus_flutter.feature.pose
 
 class PoseResult {
+    // TODO: 12/4/22 Use kotlin-result
+    val isSuccess get() = success != null
     var success: PoseSuccess? = null
     var failure: PoseError? = null
 }
