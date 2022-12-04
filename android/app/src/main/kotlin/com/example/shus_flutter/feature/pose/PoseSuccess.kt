ffa3a6b63c9b1d6879b98d87666f5cf0f1085056
Shuhei Yoshida <aneki525@gmail.com>
2022-12-04 15:59:48 +0900

    update pose success

--- android/app/src/main/kotlin/com/example/shus_flutter/feature/pose/PoseSuccess.kt
+++ android/app/src/main/kotlin/com/example/shus_flutter/feature/pose/PoseSuccess.kt
@@ -1,4 +1,27 @@
 package com.example.shus_flutter.feature.pose
 
-class PoseSuccess {
+import android.graphics.Bitmap
+import com.example.shus_flutter.common.`interface`.NativeSuccess
+import com.example.shus_flutter.common.data.FlutterImage
+import com.google.mlkit.vision.pose.Pose
+import java.util.*
+
+class PoseSuccess : NativeSuccess {
+    private val renderer = DrawRenderer()
+    private val flutterImage: FlutterImage
+
+    constructor(pose: Pose, bitmap: Bitmap) {
+        val rendered: Bitmap = renderer.draw(pose, bitmap)
+        flutterImage = FlutterImage(rendered)
+    }
+
+    override val isSuccess: Boolean
+        get() = true
+
+    override fun toData(): HashMap<String, Any> {
+        var result = HashMap<String, Any>()
+        result.put("result", isSuccess)
+        result.put("image", flutterImage.toData())
+        return result
+    }
 }
