1378722c0927c0539634c67bef14461d4528634d
Shuhei Yoshida <aneki525@gmail.com>
2022-12-04 21:19:50 +0900

    add line paint style

--- android/app/src/main/kotlin/com/example/shus_flutter/feature/pose/DrawRenderer.kt
+++ android/app/src/main/kotlin/com/example/shus_flutter/feature/pose/DrawRenderer.kt
@@ -10,7 +10,7 @@ class DrawRenderer {
     private val circleFillPaint: Paint = Paint()
     private val circleBorderPaint: Paint = Paint()
     private val linePaint: Paint = Paint()
-    private val circleRadius: Float = 16f
+    private val circleRadius: Float = 8f
 
     constructor() {
         circleBorderPaint.style = Paint.Style.STROKE
@@ -19,10 +19,11 @@ class DrawRenderer {
 
         circleFillPaint.style = Paint.Style.FILL
         circleFillPaint.color = Color.WHITE
-
         circleBorderPaint.style = Paint.Style.STROKE
+
         linePaint.color = Color.argb(255, 148, 209, 106)
         linePaint.strokeWidth = 4f
+        linePaint.style = Paint.Style.STROKE
     }
 
     fun draw(pose: Pose, bitmap: Bitmap): Bitmap {
@@ -33,7 +34,7 @@ class DrawRenderer {
             val start = positions.first
             val end = positions.second
 
-            canvas.drawLine(start.position.x, start.position.y, start.position.x, start.position.y, linePaint)
+            canvas.drawLine(start.position.x, start.position.y, end.position.x, end.position.y, linePaint)
             canvas.drawCircle(start.position.x, start.position.y, circleRadius, circleBorderPaint)
             canvas.drawCircle(end.position.x, end.position.y, circleRadius, circleBorderPaint)
             canvas.drawCircle(start.position.x, start.position.y, circleRadius, circleFillPaint)
